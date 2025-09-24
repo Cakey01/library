7827fafb3499d2dc42c9448cfc58598e099c15e9
Cakey01 <alexanderwarren7@gmail.com>
2025-09-24 14:05:19 -0400

    Clean up code and add remove button for book cards

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // library array
 const library = [];
 
+    // variables
+
+
+// function declarations
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -14,63 +18,52 @@ function addBookToLibrary(library) {
     const author = document.getElementById('author');
     const pages = document.getElementById('pages');
     const read = document.getElementById('read');
-
     const book = new Book(title.value, author.value, pages.value, read.value);
+
     library.push(book);
-    resetInputs();
-    console.log(library);
-    reset();
-    show(library);
 }
 
 function resetInputs() {
-    title.value = ''
-    author.value = ''
-    pages.value = ''
-    read.value = 'Want to read'
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    read.value = 'Want to read';
 }
 
+function removeCard(library) {
+    const remove = document.querySelectorAll('.remove');
+    remove.forEach(button => {
+        button.addEventListener('click', () => {
+            const card = button.parentElement;
+            const id = card.dataset.id;
+            const toDelete = library.find(book => book.id === id);
+            const index = library.indexOf(toDelete);
+            library.splice(index, 1);
+            show(library);
+        })
+    })
+}
 
-// form
-const dialog = document.querySelector("dialog");
-const add = document.getElementById('add');
-const submit = document.getElementById('submit');
-const close = document.getElementById('close');
-
-
-// show dialog
-add.addEventListener('click', () => {
-    dialog.showModal();
-})
-
-//stop submit
-submit.addEventListener('click', (event) => {
-    event.preventDefault();
-    addBookToLibrary(library);
-    dialog.close();
-})
-
-// close dialog
-close.addEventListener('click', () => {
-    dialog.close();
-})
-
-// reset container before showing
-function reset() {
+// reset container before showing library
+function resetLibrary() {
     const cards = document.querySelectorAll('.card');
 
     if (!cards) {
-        return
+        return;
     } else {
-        cards.forEach(card => card.remove())
+        cards.forEach(card => card.remove());
     }
 }
 
 // show books in library
 function show(library) {
+    resetLibrary();
     library.forEach((book) => {
         const container = document.querySelector('.card-container');
         const card = document.createElement('div');
+        const remove = document.createElement('button');
+        remove.classList.add('remove');
+        remove.textContent = 'remove';
         card.classList.add('card');
 
         for (const property in book) {
@@ -83,6 +76,34 @@ function show(library) {
                 card.dataset.id = book['id'];
             }
         }
+        card.appendChild(remove);
         container.appendChild(card);
-    })
+    });
+    removeCard(library);
 }
+
+
+// dialog
+const dialog = document.querySelector('dialog');
+const add = document.getElementById('add');
+const submit = document.getElementById('submit');
+const close = document.getElementById('close');
+
+// show
+add.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+// submit
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    addBookToLibrary(library);
+    resetInputs();
+    dialog.close();
+    show(library);
+});
+
+// close
+close.addEventListener('click', () => {
+    dialog.close();
+});
